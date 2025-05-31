47a6d707f43f3c86891d367416d86077290569e1
BlackGhost-226 <bg.BlackGhost226@gmail.com>
2025-05-31 22:17:16 +0200

    build sys save

--- Assets/code/BuildingSystem/Building.cs
+++ Assets/code/BuildingSystem/Building.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Building : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float elecrisityProduse, moneyProduse;
+    public bool roadNeeded, elecrisityNeeded;
+    public int coast, person;
+
+    private bool conected = false;
+
+
+    private GridLayout grid;
+    private Tilemap mainTm;
+    private Vector3Int posInGrid;
+    private List<Tile> tilesAround = new List<Tile>();
+
+
+    void Start() {
+        // if a electric producer
+        if (elecrisityProduse > 0) {
+            conected = true;
+        }
+
+        // grid setup
+        grid = FindObjectOfType<GridLayout>();
+
+        // mainTm setup
+        foreach (var tilemap in FindObjectsOfType<Tilemap>()) {
+            if (tilemap.name == "MainTilemap"){
+                mainTm = tilemap;
+            }
+        }
+
+        // posInGrid setup
+        posInGrid = grid.WorldToCell(this.transform.position);
+
+        // tilesAround setup
+            // [*][*][*]
+            // [ ] 0 [ ]
+            // [ ][ ][ ]
+            tilesAround.Add(mainTm.GetTile<Tile>(posInGrid + new Vector3Int(0, 1, 0)));
+            tilesAround.Add(mainTm.GetTile<Tile>(posInGrid + new Vector3Int(1, 1, 0)));
+            tilesAround.Add(mainTm.GetTile<Tile>(posInGrid + new Vector3Int(-1, 1, 0)));
+
+            // [ ][ ][ ]
+            // [*] 0 [*]
+            // [ ][ ][ ]
+            tilesAround.Add(mainTm.GetTile<Tile>(posInGrid + new Vector3Int(1, 0, 0)));
+            tilesAround.Add(mainTm.GetTile<Tile>(posInGrid + new Vector3Int(-1, 0, 0)));
+
+            // [ ][ ][ ]
+            // [ ] 0 [ ]
+            // [*][*][*]
+            tilesAround.Add(mainTm.GetTile<Tile>(posInGrid + new Vector3Int(0, -1, 0)));
+            tilesAround.Add(mainTm.GetTile<Tile>(posInGrid + new Vector3Int(1, -1, 0)));
+            tilesAround.Add(mainTm.GetTile<Tile>(posInGrid + new Vector3Int(-1, -1, 0)));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        foreach (var tile in tilesAround) {
+            if (tile == null) {
+                Debug.Log(tile);
+            }
+        }
     }
+
 }
--- Assets/code/BuildingSystem/BuildingSystem.cs
+++ Assets/code/BuildingSystem/BuildingSystem.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -9,26 +11,78 @@ public class BuildingSystem : MonoBehaviour
     public GameObject iso;
     public Tilemap tempTm;
     public Tilemap mainTm;
+    public GameObject m;
 
-    private Vector3 mouse;
+    private Vector3 mousePos;
     private Vector3Int cellPos;
-    private SpriteRenderer isoRen;
     private GridLayout grid;
+    private Vector3 offset = new Vector3(0.5f, 0.5f, 0f);
+    private Dictionary<BuildTileType, Tile> buildTileDic = new Dictionary<BuildTileType, Tile>();
+    private Dictionary<BuildingTileType, GameObject> buildingTileDic = new Dictionary<BuildingTileType, GameObject>();
+    private Vector3Int oldCellPos;
+
+    private SpriteRenderer mR;
+    private SpriteRenderer isoR;
 
     void Start()
     {
         grid = this.GetComponent<GridLayout>();
-        isoRen = iso.GetComponent<SpriteRenderer>();
-        // icoRen.enabled = !icoRen.enabled;
+        mR = m.GetComponent<SpriteRenderer>();
+        isoR = iso.GetComponent<SpriteRenderer>();
+
+        mR.sprite = isoR.sprite;
+        mR.color = isoR.color;
+
+        buildTileDic.Add(BuildTileType.Empty, null);
+        buildTileDic.Add(BuildTileType.White, Resources.Load<Tile>("palette/build/white"));
+        buildTileDic.Add(BuildTileType.Red, Resources.Load<Tile>("palette/build/red"));
+        buildTileDic.Add(BuildTileType.Green, Resources.Load<Tile>("palette/build/green"));
+        buildTileDic.Add(BuildTileType.Road, Resources.Load<Tile>("palette/build/road"));
+
+        buildingTileDic.Add(BuildingTileType.Home, iso);
     }
 
     void FixedUpdate() {
-        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        cellPos = grid.WorldToCell(mouse);
-        if (mainTm.HasTile(cellPos)) {
-            Debug.Log(mainTm.GetTile<Tile>(cellPos).name);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cellPos = grid.WorldToCell(mousePos);
+
+        // visual
+        if (oldCellPos != cellPos) {
+            if (IsPlaceble()) {
+                tempTm.SetTile(cellPos, buildTileDic[BuildTileType.Green]);
+            }else {
+                tempTm.SetTile(cellPos, buildTileDic[BuildTileType.Red]);
+            }
+
+            tempTm.SetTile(oldCellPos, buildTileDic[BuildTileType.Empty]);
+            m.transform.position = grid.CellToWorld(cellPos)+offset;
+            oldCellPos = cellPos;
         }
 
-        iso.transform.position = grid.CellToWorld(cellPos);
+        // create building
+        if (Input.GetMouseButton(0) && IsPlaceble()) {
+                Instantiate(buildingTileDic[BuildingTileType.Home], grid.CellToWorld(cellPos)+offset, Quaternion.identity);
+                mainTm.SetTile(cellPos, buildTileDic[BuildTileType.Empty]);
+        }
+    }
+
+    private bool IsPlaceble() {
+        if (mainTm.GetTile<Tile>(cellPos) == buildTileDic[BuildTileType.White]) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public enum BuildTileType {
+        Empty,
+        White,
+        Red,
+        Green,
+        Road
+    }
+
+    public enum BuildingTileType {
+        Home
     }
 }
